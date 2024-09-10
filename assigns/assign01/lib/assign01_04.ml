151476e56a11fcf86db9025cf12310f556fa28fa
sdallmn <sdallman18@gmail.com>
2024-09-10 13:35:10 -0400

    auctually finished

--- assigns/assign01/lib/assign01_04.ml
+++ assigns/assign01/lib/assign01_04.ml
@@ -9,7 +9,9 @@ let to_string s =
   let rec ints str i = 
     let num = nth s i in 
     let nextnum = nth s (i+1) in
-    if num = 0 || nextnum = 0 then
+    if num = 0 then
+      "[]"
+    else if num = 0 || nextnum = 0 then
       str ^ (string_of_int num) ^ "]"
     else
       ints (str ^ (string_of_int num) ^ "; ") (i +1)
